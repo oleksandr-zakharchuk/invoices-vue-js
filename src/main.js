e00d498e10d9268b144fd0bb51d82f4b704c9bca
Oleksandr Zakharchuk <oleksandr.zakharchuk.dev@gmail.com>
2023-11-13 14:28:04 +0200

    Update backend url

--- src/main.js
+++ src/main.js
@@ -5,5 +5,5 @@ import App from './App.vue'
 import router from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://127.0.0.1:5000';
+axios.defaults.baseURL = 'https://wholesale-trade-9ce5d0228edc.herokuapp.com';
 createApp(App).use(router).mount('#app')
